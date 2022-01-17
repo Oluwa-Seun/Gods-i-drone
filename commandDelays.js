eb24866ee164aa6e30f22a29e49ed9e2f5fd83fa
JujuBug <61301594+Oluwa-Seun@users.noreply.github.com>
2022-01-17 13:27:32 +0100

    wrote logic for commands & created new file commandsDelay.js

---
+++ commandDelays.js
@@ -0,0 +1,20 @@
+const commandDelays = {
+    command: 500,
+    takeoff: 5000,
+    land: 5000,
+    up: 7000,
+    down: 7000,
+    left: 5000,
+    go: 7000,
+    right: 5000,
+    forward: 5000,
+    back: 5000,
+    cw: 5000,
+    ccw: 5000,
+    flip: 5000,
+    speed: 5000,
+    'battery?': 500,
+    'speed?': 500,
+    'time?': 500,
+};
+module.exports = commandDelays;
--- fly.js
+++ fly.js
@@ -1,5 +1,6 @@
 const dgram = require('dgram');
 const wait = require('wait');
+const commandDelays = require('.commandDelays');
 
 const PORT = 8000;
 const HOST = '192.168.10.1';
@@ -15,5 +16,26 @@ function handleError(err) {
     if (err) {
         console.log('ERROR');
         console.log(err);
+    }
+}
+
+drone.send('command', 0, 7, PORT, HOST, handleError);
+drone.send('battery?', 0, 8, PORT, HOST, handleError);
+
+// drone.send('command', 0, 7, PORT, HOST, handelError);
+//drone.send('battery?', 0, 8, PORT, HOST, handelError);
+
+const commands = ['commands', 'battery?', 'takeoff', 'land'];
+
+let i = 0;
+
+async function go() {
+    const command = commands[i];
+
+    const delay = commandDelays[command];
+    console.log(`running command: ${command}`);
+    drone.send(command, 0, command.lenght, PORT, HOST, handleError)
+    if (i < commands.length) {
+        return go()
     }
 }
